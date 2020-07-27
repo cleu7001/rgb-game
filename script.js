6412df147762961034ad311eb045f884e31df54d
cleu7001 <cleu7001@gmail.com>
2020-07-27 23:20:53 +1000

    Fixed bug for incorrect squares click behaviour
    Fixed bug which prevented squares from disappearing when incorrect squares are clicked.

--- script.js
+++ script.js
@@ -40,7 +40,7 @@ function setupSquares() {
 				changeColors(clickedColor);
 				h1.style.backgroundColor = clickedColor;
 			} else{
-				this.style.backgroundColor = "#steelblue";
+				this.style.backgroundColor = "#232323";
 				messageDisplay.textContent = "Try Again"
 			}
 		});
